96e4280826ab69f0f03029964725a28c9bd5018f
JANGEUNKYUNG <jek10322@naver.com>
2023-03-27 12:12:58 +0900

    for 문 출력 하기

--- VR_GameProject_2/Assets/Scripts/0320/Test_001.cs
+++ VR_GameProject_2/Assets/Scripts/0320/Test_001.cs
@@ -9,18 +9,18 @@ public class Test_001 : MonoBehaviour
     {
         //주석 공간은 스페이스가 아니라 TAB
 
-        int age;                       //정수형 int 선언 이름은 age
-        age = 30;                      //age에 30을 입력 (=)은 입력
-        Debug.Log(age);                //Conso.log 창에 age 의 값을 보여준다.
-                                       
-        float height1 = 160.5f;        //부동소수점형 float 선언 이름은 height1 이고 160.5f 값 입력
-        float height2;                  //float 형 height2 선언 (입력 값은 따로 없음 NULL)
-        height2 = height1;             //height1에 있는 height2에 입력
-        Debug.Log(height2);            //Console.log 창에 height2 의 값을 보여준다.
+        int age;                            //정수형 int 선언 이름은 age
+        age = 30;                           //age 에 30을 입력 (=)은 입력
+        Debug.Log(age);                     //Consle.log 창에 age 의 값을 보여준다.
 
-        string name;                  //문자열 name 선언
-        name = "Sera";                //name에 Sare 입력
-        Debug.Log(name);              //Console.log 창에 name 의 값을 보여준다.
+        float height1 = 160.5f;             //부동소수점형 float 선언 이름은  height1 이고 160.5f 값 입력
+        float height2;                      //float 형 height2 선언 (입력 값은 따로 없음 NULL)
+        height2 = height1;                  //height1에 있는 height2에 입력
+        Debug.Log(height2);                 //Consle.log 창에 height2 의 값을 보여준다.
+
+        string name;                        //문자열 name 선언
+        name = "Sera";                      //name에 Sera 입력
+        Debug.Log(name);                    //Console.log 창에 name 의 값을 보여준다.
     }
 
     // Update is called once per frame
--- VR_GameProject_2/Assets/Scripts/0320/Test_002.cs
+++ VR_GameProject_2/Assets/Scripts/0320/Test_002.cs
@@ -7,21 +7,21 @@ public class Test_002 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int answer;                     //변수 int answer 선언
-        answer = 1 + 2;                 //answer에 1 + 2 값 입력
-        Debug.Log(answer);              //Console.log() answer값을 보여줌
+        int answer;
+        answer = 1 + 2;
+        Debug.Log(answer);
 
-        int n1 = 8;                   //변수 int 선언 이름은 n1이고 8의 값을 입력
-        int n2 = 9;                   //변수 int 선언 이름은 n1이고 9의 값을 입력
-        int answer2;                  //변수 int answer2 선언 (기존에 answer 선언 되어있어서 다시 선언)
-        answer2 = n1 + n2;            //answer2에 변수 n1 값과 n2 값의 더한 값을 입력
-        Debug.Log(answer2);           //Console.log() answer2 값을 보여줌
+        int n1 = 8;
+        int n2 = 9;
+        int answer2;
+        answer2 = n1 + n2;
+        Debug.Log(answer2);
 
-        answer2 += 5;                 //answer2 = answer2 + 5의 축약
-        Debug.Log(answer2);           //Console.log() answer2 값을 보여줌
-                              
-        answer2++;                    //answe2 = answer2 + 1의 축약 보통 반복문에서 1씩 더해서 처리할때 쓰임
-        Debug.Log(answer2);           //Console.log() answer2 값을 보여줌
+        answer2 += 5;
+        Debug.Log(answer2);
+
+        answer2++;
+        Debug.Log(answer2);
     }
 
     // Update is called once per frame
